4a765b7edd34773b40533a383ace102e9c2b3ba7
casho <aseelhussein@aol.com>
2022-08-14 20:23:58 -0400

    need toString

--- src/ArrayIntro/ArrayPrac1.java
+++ src/ArrayIntro/ArrayPrac1.java
@@ -1,8 +1,17 @@
 package ArrayIntro;
 
+import java.util.Arrays;
+
 public class ArrayPrac1 {
 
+    public static void main(String[] args) {
 
+        int [] scores = new int[5];
+
+
+
+
+    }
 
 
 }
