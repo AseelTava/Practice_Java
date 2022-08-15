289be295396b50044026b79a7300b7dc265b6458
casho <aseelhussein@aol.com>
2022-08-14 20:30:31 -0400

    Add Arraies

--- src/ArrayIntro/ArrayPrac1.java
+++ src/ArrayIntro/ArrayPrac1.java
@@ -7,6 +7,15 @@ public class ArrayPrac1 {
     public static void main(String[] args) {
 
         int [] scores = new int[5];
+        scores[2] = 78;
+        scores[0] = 85;
+        scores[1] = 95;
+        scores[3]= 88;
+        scores[4] = 75;
+
+        System.out.println(Arrays.toString(scores));
+
+
 
 
 
