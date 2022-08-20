0bb15d11410036f14f06ffd5c791a2794fac43d5
casho <aseelhussein@aol.com>
2022-08-19 21:13:12 -0400

    odd numbers completed

--- src/Loop/Odd_number_for_each_loop.java
+++ src/Loop/Odd_number_for_each_loop.java
@@ -2,4 +2,18 @@ package Loop;
 
 public class Odd_number_for_each_loop {
 
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+        for (int each : numbers) {
+
+            if (each % 2 != 0) { // find odd numbers
+                System.out.println(each);
+            }
+
+        }
+
+    }
 }
