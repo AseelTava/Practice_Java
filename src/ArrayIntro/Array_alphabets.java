4683b3e817212387270300e32124078d8ff369b1
casho <aseelhussein@aol.com>
2022-08-19 20:58:08 -0400

    Array Alphabets - Print called

--- src/ArrayIntro/Array_alphabets.java
+++ src/ArrayIntro/Array_alphabets.java
@@ -8,6 +8,15 @@ public class Array_alphabets {
         char[] ascending = new char[26]; // index: 0~25
         char[] descending = new char[26]; // index: 0~25
 
+
+        for (int i = 0, j = 'A', k ='Z' ; i < ascending.length; i++, j++, k--) {
+            ascending[i] = (char) j; // A ~ Z
+            descending[i] = (char) k; // Z ~ A
+        }
+
+        System.out.println(Arrays.toString(ascending));
+        System.out.println(Arrays.toString(descending));
+
     }
 
     }
