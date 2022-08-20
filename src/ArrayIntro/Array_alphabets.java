a0bf6b76d96649557f629e731dc16ac132becaf7
casho <aseelhussein@aol.com>
2022-08-19 20:57:09 -0400

    Array Alphabets created

---
+++ src/ArrayIntro/Array_alphabets.java
@@ -0,0 +1,13 @@
+package ArrayIntro;
+
+import java.util.Arrays;
+
+public class Array_alphabets {
+    public static void main(String[] args) {
+
+        char[] ascending = new char[26]; // index: 0~25
+        char[] descending = new char[26]; // index: 0~25
+
+    }
+
+    }
