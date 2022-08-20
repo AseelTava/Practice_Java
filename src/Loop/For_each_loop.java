f03615a31d5d81c167169d3e6d17839deb99ad17
casho <aseelhussein@aol.com>
2022-08-19 21:10:38 -0400

    Use ForEach Loop task

---
+++ src/Loop/For_each_loop.java
@@ -0,0 +1,14 @@
+package Loop;
+
+public class For_each_loop {
+    public static void main(String[] args) {
+
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        for (int number : numbers) {   // for each loop
+            System.out.println(number); // simply print variable numbers
+        }
+    }
+
+}
