48c27e64b6d55122a1df3f58fd3c5bb3750a5b21
casho <aseelhussein@aol.com>
2022-08-19 21:08:30 -0400

    Use ForEach Loop

--- src/Loop/ForEachLoop.java
+++ src/Loop/ForEachLoop.java
@@ -4,10 +4,10 @@ public class ForEachLoop {
 
     public static void main(String[] args) {
 
-        // use for loop to solve the task
+
         int[] numbers = {10, 20, 30, 40, 50};
 
-        for (int number : numbers) {
+        for (int number : numbers) {   // for each loop
             System.out.println(number);
         }
     }
