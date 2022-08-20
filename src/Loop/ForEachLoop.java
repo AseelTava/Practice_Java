f4cebcae1ea0a3642d1410598fda611b4a7da6f6
casho <aseelhussein@aol.com>
2022-08-19 21:07:38 -0400

    Use ForEach Loop

--- src/Loop/ForEachLoop.java
+++ src/Loop/ForEachLoop.java
@@ -1,4 +1,20 @@
 package Loop;
 
 public class ForEachLoop {
+
+    public static void main(String[] args) {
+
+        // use for loop to solve the task
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+
 }
+
+
+
+
+
