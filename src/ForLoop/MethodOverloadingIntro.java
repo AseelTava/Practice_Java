3565aa72d9d61d88083f8fe95e71ab49238c18c7
casho <aseelhussein@aol.com>
2022-09-17 19:59:39 -0400

    For Loop Practice

---
+++ src/ForLoop/MethodOverloadingIntro.java
@@ -0,0 +1,25 @@
+package ForLoop;
+
+public class MethodOverloadingIntro {
+
+
+    public static void methodA(){
+
+    }
+
+    public static void methodA(int a){
+
+    }
+
+
+    public static void methodA(double b){
+
+    }
+
+
+    public static void methodA(int a, int b){
+
+    }
+
+
+}
